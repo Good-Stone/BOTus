d1e7aaab49a55a455a23a2209928a282e0f8a447
giooooo <gbperalta1@up.edu.ph>
2017-07-05 12:09:16 +0800

    Discord API integration

--- src/api/bot/bot.class.js
+++ src/api/bot/bot.class.js
@@ -1,8 +1,11 @@
 
+import Discord from 'discord.js';
+
 const log = require('debug')('bot');
 
-export default class Bot {
-  static start() {
-    log('IM ALIVE');
+export default class Bot extends Discord.Client {
+  contructor() {
+    super.login(process.env.DISCORD_TOKEN);
+    log('Im Alive!');
   }
 }
