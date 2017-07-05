9f40fcb536947188392d9d7c79603b0abedc67f0
septa97 <jancharles.adona@gmail.com>
2017-07-05 20:35:27 +0800

    Added everyday greetings (morning, noon, and evening)

--- src/api/bot/bot.event-functions.js
+++ src/api/bot/bot.event-functions.js
@@ -1,8 +1,11 @@
+import Botus from './bot.class';
 import {
   WELCOME_MESSAGE,
   COMMAND_RANDOMIZE,
+  GREETING_MORNING,
+  GREETING_NOON,
+  GREETING_EVENING,
 } from './bot.constants';
-
 import {
   randomize,
   parseMessage,
@@ -12,6 +15,36 @@ const log = require('debug')('bot.event-functions');
 
 export function onReady() {
   log(`${WELCOME_MESSAGE} Nigga im alive`);
+
+  // Loop for checking the current time
+  setInterval(() => {
+    const currentTime = new Date().toLocaleTimeString().split(':');
+    const hour = parseInt(currentTime[0], 10);
+    const minute = parseInt(currentTime[1], 10);
+    const second = parseInt(currentTime[2], 10);
+    log(hour, minute, second);
+
+    // Morning, Noon, and Evening greeting
+    if (hour === 7 && minute === 0 && second === 0) {
+      Botus.channels.forEach((channel) => {
+        if (channel.type === 'text') {
+          channel.send(GREETING_MORNING);
+        }
+      });
+    } else if (hour === 12 && minute === 0 && second === 0) {
+      Botus.channels.forEach((channel) => {
+        if (channel.type === 'text') {
+          channel.send(GREETING_NOON);
+        }
+      });
+    } else if (hour === 19 && minute === 0 && second === 0) {
+      Botus.channels.forEach((channel) => {
+        if (channel.type === 'text') {
+          channel.send(GREETING_EVENING);
+        }
+      });
+    }
+  }, 1000);
 }
 
 export function onReceivedMessage(message) {
