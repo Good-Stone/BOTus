47328629af9108e32cbdce80a3c5e9793586d185
septa97 <jancharles.adona@gmail.com>
2017-07-07 09:34:27 +0800

    Rebuild dist folder

--- dist/api/bot/bot.event-functions.js
+++ dist/api/bot/bot.event-functions.js
@@ -24,7 +24,7 @@ var log = require('debug')('bot.event-functions');
 
 function onReady() {
   log(_bot3.WELCOME_MESSAGE + ' Nigga im alive');
-  _bot2.default.user.setGame('with your mom');
+  _bot2.default.user.setGame('with my Dog');
 
   // Loop for checking the current time
   setInterval(function () {
